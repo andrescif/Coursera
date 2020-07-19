3c76a483c88e780a0f210c6755a4d51fae706aff
Andres Cifuentes <andrescif@gmail.com>
2020-07-18 18:42:38 -0600

    Completada la tarea de Logica de Swirl.  Ahora por la de fecha y hora.

---
+++ Tareas_swirl.R
@@ -0,0 +1,51 @@
+install.packages("swirl")
+library(swirl)
+ls()
+swirl()
+
+TRUE==TRUE
+(FALSE==TRUE)==FALSE
+6==7
+6<7
+10<=10
+4
+2
+5!=7
+!(5==7)
+3
+4
+FALSE & FALSE
+TRUE & c(TRUE,FALSE,FALSE)
+TRUE && c(TRUE,FALSE,FALSE)
+TRUE | c(TRUE,FALSE,FALSE)
+TRUE || c(TRUE,FALSE,FALSE)
+5>8 || 6 !=8 && 4>3.9
+1
+2
+4
+isTRUE(6>4)
+1
+identical('twins', 'twins')
+1
+xor(5==6,!FALSE)
+2
+ints <- sample(10)
+ints
+ints>5
+which(ints>7)
+2
+any(ints<0)
+all(ints>0)
+4
+1
+andrescif@gmail.com
+#8Oog31IjMNTyV3iN
+
+1
+9
+Sys.Date()
+mean(c(2,4,5))
+
+
+
+
