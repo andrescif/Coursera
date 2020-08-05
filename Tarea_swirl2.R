663a367cb1ff0804e0a7cc925d215eaa8e8a2621
Andres Cifuentes <andrescif@gmail.com>
2020-08-04 22:01:07 -0600

    Finalizada la tarea y el quiz de esta parte del curso.

--- Tarea_swirl2.R
+++ Tarea_swirl2.R
@@ -36,10 +36,54 @@ sapply(flags,unique)
 lapply(unique_vals,function(elem) elem[2])
 2
 andrescif@gmail.com
-1coKCiWZR6N9oud9
+#1coKCiWZR6N9oud9
 
+1
+11
+sapply(flags,unique)
+vapply(flags,unique,numeric(1))
+ok()
+sapply(flags,class)
+vapply(flags,class,character(1))
+?tapply
+table(flags$landmass)
+table(flags$animate)
+tapply(flags$animate,flags$landmass,mean)
+tapply(flags$population,flags$red,summary)
+1
+tapply(flags$population,flags$landmass,summary)
+3
+1
+andrescif@gmail.com
+#OqxAldGzFOiD4PGJ
+0
+#Quiz
+library(datasets)
+data(iris)
+?iris
+iris$Sepal.Length
+tapply(iris$Sepal.Length,iris$Species,mean)
+apply(iris[,1:4],2,mean)
+library(datasets)
+data(mtcars)
+with(mtcars,tapply(mpg,cyl,mean))
+tapply(mtcars$cyl,mtcars$mpg,mean)
+sapply(split(mtcars$mpg,mtcars$cyl),mean)
+mean(mtcars$mpg,mtcars$cyl)
+lapply(mtcars,mean)
+tapply(mtcars$mpg,mtcars$cyl,mean)
+apply(mtcars,2,mean)
+sapply(mtcars,cyl,mean)
 
-
-
-
+tapply(mtcars$hp,mtcars$cyl,mean)
+82.63636- 122.28571
+debug(ls)
+ls()
+2
+1
+4
+2
+3
+4
+5
 
